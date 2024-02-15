fe416ac807fcf912f71e56175ddb462d219fe898
hjali7 <hjikingali@gmail.com>
2024-02-15 14:38:40 +0330

    set platform and detectCollision for platform and doodler

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const context = board.getContext("2d");
 // doodler var
 const doodlerWidth = 46;
 const doodlerHeight = 46;
-let doodlerX = board.width * 2 + doodlerWidth /3 ;
-let doodlerY = board.height * 3;
+let doodlerX = board.width * 3 + doodlerWidth /3 ;
+let doodlerY = board.height * 5;
 
 // doodler obj
 let doodler = {
@@ -21,7 +21,16 @@ let doodler = {
 // game physics
 
 let velacityX = 0;
-let gamePuase = false;
+let velacityY = 0;
+const initialVelacity = -4;
+const gravity = 0.4;
+
+// platforms
+
+const platformWidth = 60;
+const platformHeight = 20;
+let platformImg;
+let platformArrays = [];
 
 window.onload = () => {    
     const boardHeight = board.height = window.innerHeight;
@@ -35,6 +44,11 @@ window.onload = () => {
     doodler.img = doodlerLeftImg;
 
     doodlerLeftImg.onload = () => context.drawImage(doodler.img , doodler.x , doodler.y , doodler.width , doodler.height);
+
+    platformImg = new Image();
+    platformImg.src = "./assets/platform.png";
+    velacityY = initialVelacity;
+    DrawPlatforms();
     
     requestAnimationFrame(update);
   
@@ -52,14 +66,71 @@ window.onload = () => {
 };
 
 function update () {
-        requestAnimationFrame(update);
-        context.clearRect(0, 0, board.width, board.height);
-        doodler.x += velacityX;
-        context.drawImage(
-          doodler.img,
-          doodler.x,
-          doodler.y,
-          doodler.width,
-          doodler.height
-        );
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, board.width, board.height);
+    
+    doodler.x += velacityX;    
+    if(doodler.x > board.width ) {
+        doodler.x = 0 ;
+    }else if (doodler.x + doodler.width < 0 ) {
+        doodler.x = board.width ;
+    };
+
+    velacityY += gravity;
+    doodler.y += velacityY;
+    
+    context.drawImage(doodler.img,doodler.x,doodler.y,doodler.width,doodler.height);
+
+    for(let i = 0 ; i < platformArrays.length ; i++ ) {
+        let platform = platformArrays[i];
+        if(detectCollision(doodler , platform) && velacityY >= 0) velacityY = initialVelacity;
+        context.drawImage(platform.img , platform.x , platform.y , platform.width , platform.height);
+    };
+};
+
+function DrawPlatforms () {
+    for(let i = 0 ; i < 10 ; i++) {
+        let RandomX = Math.floor(Math.random() * board.width *3);
+        let platform = {
+            img : platformImg,
+            x : board.width + doodler.width,
+            y : board.height - 75*i - 150,
+            width : platformWidth ,
+            height : platformHeight,
+        };
+
+        let platformTwo = {
+          img: platformImg,
+          x: board.width/3,
+          y: board.height - 75 * i - 150,
+          width: platformWidth,
+          height: platformHeight,
+        };
+        let platformThree = {
+          img: platformImg,
+          x: board.width/4,
+          y: board.height - 75 * i - 150,
+          width: platformWidth,
+          height: platformHeight,
+        };
+        let platformFour = {
+          img: platformImg,
+          x: board.width / RandomX*(i+1),
+          y: board.height - 75 * i - 150,
+          width: platformWidth,
+          height: platformHeight,
+        };
+
+        platformArrays.push(platform);
+        platformArrays.push(platformTwo);
+        platformArrays.push(platformThree);
+        platformArrays.push(platformFour);
+    }
+};
+
+function detectCollision (a , b) {
+    return a.x < b.x + b.width &&
+           a.x + a.width > b.x &&
+           a.y < b.y + b.height &&
+           a.y + a.height > b.y;
 };
